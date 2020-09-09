9c6cf12a0e9f58788205beab341e3f098926be52
Nathan Cornell <nc_2@hotmail.com>
2020-09-09 16:20:13 -0400

    added schema and seeds sql. Created database in seeds.sql, inserting values into

---
+++ seeds.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE employees_DB;
+
+USE employees_db;
+
+INSERT INTO department (id, name)
+VALUES (1, "Retail");
+
+INSERT INTO department (id, name)
+VALUES (2, "Brand Experience");
+
+INSERT INTO department (id, name)
+VALUES (3, "Human Resources");
+
+INSERT INTO department (id, name)
+VALUES (4, "IT");
