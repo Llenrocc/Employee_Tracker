cee237cc81e363a0bf587443dfe265646a12382c
Nathan Cornell <nc_2@hotmail.com>
2020-09-21 17:08:22 -0400

    added query to add role and department

---
+++ app.js
@@ -0,0 +1,349 @@
+const mysql = require("mysql");
+const ingquirer = require("inqiurer");
+const consoleTable = require("console.table");
+const promisemysql = require("promise-mysql");
+const inquirer = require("inquirer");
+
+const connectionProperties = {
+    host: "localhost",
+    port: 3001,
+    user: "root",
+    password: "118balliol",
+    databse: "employees_DB"
+}
+
+const connection = mysql.createConnection(connectionProperties);
+
+connection.connect((err) => {
+    if (err) throw err;
+
+    console.log("\n Employee Tracker \n");
+    mainMenu();
+});
+
+
+function mainMenu() {
+    inquirer.prompt ({
+        name:"action",
+        type: "list",
+        message: "Main Menu",
+        choices: [
+            "View employees",
+            "View employees by department",
+            "View employees by manager",
+            "View employees by role",
+            "Add employee",
+            "Add role",
+            "Add department",
+            "Update employee manager",
+            "Update employee role",
+            "Delete department",
+            "Delete employee",
+            "Delete role",
+        ]
+    })
+    .then((answer) => {
+
+        // switch the case depending on user choice
+        switch (answer.action) {
+            case "View employees":
+                viewEmp();
+                break;
+
+            case "View employees by department":
+                viewEmpByDept();
+                break;  
+            
+            case "View employees by manager":
+                viewEmpByMngr();
+                break;
+
+            case "View employees by role":
+                viewEmpByRole();
+                break;
+
+            case "Add employee":
+                addEmp();
+                break;
+
+            case "Add role":
+                addRole();
+                break;
+
+            case "Add department":
+                addDep();
+                break;
+
+            case "Update employee manager":
+                updateEmpMngr();
+                break;
+
+            case "Update employee role":
+                updateEmpRole();
+                break;
+
+            case "Delete depeartment":
+                deleteDept();
+                break;
+
+            case "Delete employee":
+                deleteEmp();
+                break;
+
+            case "Delete role":
+                deleteRole();
+                break;
+        }
+    });
+}
+
+function viewEmp() {
+
+    // query to view employees
+    let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concay(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id=m.id INNER JOIN ON e.role_id=role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
+
+    connection.query(query, function(err, res) {
+        if (err) return err;
+        console.log("\n");
+        console.table(res);
+
+        mainMenu();
+    })
+}
+
+function viewEmpByDept() {
+
+    let deptArr = [];           // global array for dept titles
+
+    promisemysql.createConnection(connectionProperties)
+    .then((conn) => {
+
+
+        return conn.query('SELECT name FROM department');
+    }).then(function(value) {
+        deptQuery = value;
+        for (i=0; i < value.length; i++) {
+            deptArr.push(value[i].name);
+        }
+    }).then(() => {
+        inquirer.prompt({
+            name: "department",
+            type: "list",
+            message: "Which department would you like to search?",
+            choices: deptArr
+        })
+        .then((answer) => {
+
+            const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = '${answer.department}' ORDER BY ID ASC`;
+            connection.query(query, (err, res) => {
+                if (err) return err;
+
+                console.log("\n");
+                console.table(res);
+
+                mainMenu();
+            });
+        });
+    });
+}
+
+function viewEmpByRole() {
+    let roleArr = [];
+
+    promisemysql.createConnection(connectionProperties)
+    .then((conn) => {
+        return conn.query('SELECT title FROM role');
+    }).then(function(roles) {
+        for (i=0; i < roles.length; i++) {
+            roleArr.push(roles[i].title);
+        }
+    }).then(() => {
+        inquirer.prompt({
+            name: "role",
+            type: "list",
+            message: "Which role do you want to search?",
+            choices: roleArr
+        })
+        .then((answer) => {
+
+            const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE role.title = '${answer.role}' ORDER BY ID ASC`;
+            connection.query(query, (err, res) => {
+                if (err) return err;
+
+                console.log("\n");
+                console.table(res);
+                mainMenu();
+            });
+        });
+    });
+}
+
+//Add employee and 2 arrays
+
+function addEmp() {
+    let roleArr = [];
+    let managerArr = [];
+
+// create connection
+
+promisemysql.createConnection(connectionProperties
+    ).then((conn) => {
+
+        return Promise.all([
+            conn.query('SELECT id, title FROM role ORDER BY title ASC'),
+            conn.query("SLECT employee.id, concat(employee.first_name, ' ' , employee.last_name) AS Employee FROM employee ORDER BY Employee ASC")
+        ]);
+    }).then(([roles, managers]) => {
+
+        for (i=0; i < roles.length; i++) {
+            roleArr.push(roles[i].title);
+        }
+
+        for (i=0; i < managers.length; i++) {
+            managerArr.push(managers[i].Employee);
+        }
+
+        return Promise.all([roles, managers]);
+    }).then(([roles, managers]) => {
+
+        managerArr.unshift('--');
+
+        //prompt user for first name
+
+        inquirer.prompt([
+            {
+                name: "firstName",
+                type: "input",
+                message: "First name: ",
+                
+                validate: function(input) {
+                    if (input === "") {
+                        console.log("REQUIRED");
+                        return false;
+                    }
+                    else {
+                        return true;
+                    }
+                }
+            },
+            {
+            
+        // prompt user for last name
+                name: "lastName",
+                type: "input",
+                message: "LastNname name: ",
+                
+                validate: function(input) {
+                    if (input === "") {
+                        console.log("REQUIRED");
+                        return false;
+                    }
+                    else {
+                        return true;
+                    }
+                }
+            },
+
+        // prompt user for role
+        
+            {
+                name: "role",
+                type: "list",
+                message: "What is their role?",
+                choices: roleArr
+            }, {
+        
+        // prompt user for manager
+                name: "manager",
+                type: "list",
+                message: "Who is their manager?",
+                choices: managerArr        
+            }]).then((answer) => {
+
+                let roleID;
+
+                let managerID = null;
+
+        // id for role selected
+                for (i=0; i < roles.length; i++) {
+                    if (answer.role == roles[i].title) {
+                        roleID = roles[i].id;
+                    }
+                }
+
+        // id for manager selected
+                for (i=0; i < managers.length; i++) {
+                    if (answer.manager == managers[i].Employee) {
+                        managerID = managers[i].id;
+                    }
+                }        
+                
+                connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                VALUES ("${answer.firstName}", "${answer.lastName}", ${roleID}, ${managerID})`, (err, res) => {
+                    if(err) return err;
+
+                    console.log(`\n EMPLOYEE ${answer.firstName} ${answer.lastName} ADDED...\n`);
+                    mainMenu();
+                });
+            });
+    });
+}
+
+function addRole() {
+
+    let departmentArr = [];
+
+    promisemysql.createConnection(connectionProperties)
+    .then((conn) => {
+
+        return conn.query('SELECT id, name FROM department ORDER BY name ASC');
+    }).then((departments) => {
+
+        for (i=0; i < departments.length; i++) {
+            departmentArr.push(departments[i].name);
+        }
+
+        
+        return departments;
+    }).then((departments) => {
+
+        inquirer.prompt([
+            {
+                // prompt user role title
+                name: "roleTitle",
+                type: "input",
+                message: "Role title: "
+            },
+            {
+                // prompt user for salary
+                name: "salary",
+                type: "number",
+                message: "Salary: "
+            },
+            {
+                // prompt user to select department role
+                name: "dept",
+                type: "list",
+                message: "Department: ",
+                choices: departmentArr
+            }]).then((answer) => {
+
+                let deptID;
+
+                for (i=0; i < departments.length; i++) {
+                    if (answer.dept == departments[i].name) {
+                        dept.ID = departments[i].id;
+                    }
+                }
+
+                connection.query(`INSERT INTO role (title, salary, department_id)
+                VALUES ("${answer.roleTitle}", ${answer.salary}, ${deptID})`, (err, res) => {
+                    if(err) return err;
+                    console.log(`\n ROLE ${answer.roleTitle} ADDED...\n`);
+                    mainMenu();
+                });
+            });
+    });
+}
+
+// Add a department
--- seeds.sql
+++ seeds.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE employees_DB;
-
 USE employees_db;
 
 INSERT INTO department (id, name)
@@ -42,3 +40,35 @@ VALUES (8, "IT Coordinator", 52000, 4);
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (9, "IT Manager", 105000, 4);
+
+
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (4, "Moses", "Malone", 3, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (5, "Fisher", "Coleman", 4, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (7, "Declan", "Mulqueen", 6, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (10, "Jonathan", "Major", 9, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Melissa", "Morris", 2, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Sharon", "Moseley", 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Vernon", "Mercer", 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (6, "Way-Too", "Soon", 5, 7);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (8, "Poris", "Horton", 7, 10);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (9, "Edgar", "Shilling", 8, 10);
